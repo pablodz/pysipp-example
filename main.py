0373905b569fef9d4572f0f279a9e55b66a745ab
Pablo Diaz <pablo.diazv@pucp.edu.pe>
2022-05-26 19:37:37 -0500

    v1

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pysipp
 
 pysipp.utils.log_to_stderr("DEBUG")
 
-uac = pysipp.client(destaddr=("pip.sip.vozy.co", 5060))
-uac.uri_username = "pdiaz"
-uac.auth_password = "BcCR2vW2ajSC"
+uac = pysipp.client(destaddr=("domain.example", 5060))
+uac.uri_username = "username"
+uac.auth_usernmame = "username"
+uac.auth_password = "password"
 uac()
